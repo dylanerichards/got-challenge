89a004465af9d2600855e4efca551989db0b5dad
Dylan Richards <dylanerichards@gmail.com>
2017-10-02 16:42:45 -0400

    Typecast born years to integers for compiarison in PG

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,7 +5,7 @@ class Character < ApplicationRecord
 
   def born_within_years_of(years)
     years = years.to_i
-    Character.where("(characters.born BETWEEN ? AND ?) OR (characters.born BETWEEN ? AND ?)", born.to_i - years, born.to_i - 1, born.to_i + 1, born.to_i + years)
+    Character.where("(CAST(characters.born AS INTEGER) BETWEEN ? AND ?) OR (CAST(characters.born AS INTEGER) BETWEEN ? AND ?)", born.to_i - years, born.to_i - 1, born.to_i + 1, born.to_i + years)
   end
 
 end
