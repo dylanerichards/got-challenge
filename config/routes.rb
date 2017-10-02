09565f55fa816a46a8bf273ddb5f8aceee1b407d
Dylan Richards <dylanerichards@gmail.com>
2017-10-02 14:36:07 -0400

    Set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :characters
 
+  root "characters#index"
+
   get "/years/:year", to: "characters#by_year", as: "year"
 
   get "/houses/:house", to: "characters#by_house", as: "house"
